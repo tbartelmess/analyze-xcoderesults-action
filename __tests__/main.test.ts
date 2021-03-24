7e903ee1d27c20b3ab5eb9afddaec7144587649a
Thomas Bartelmess <tbartelmess@marketcircle.com>
2021-03-24 18:51:30 -0400

    Initial test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,6 +2,7 @@ import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import * as xcresultool from '../src/xcresulttool'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
@@ -9,20 +10,9 @@ test('throws invalid number', async () => {
 })
 
 test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
+  console.log("hello World");
+  await xcresultool.transformXCodeResults("./TestResultsMac.xcresult");
+
 })
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
-})
+
---
+++ src/xcresulttool.ts
@@ -0,0 +1,27 @@
+import * as exec from '@actions/exec'
+import {ExecOptions} from '@actions/exec/lib/interfaces'
+
+export async function transformXCodeResults(
+  file: string
+): Promise<void> {
+
+  let output = '';
+  const options: ExecOptions = {};
+  options.listeners = {
+    stdout: (data: Buffer) => {
+      output += data.toString();
+    },
+  };
+  options.silent = true;
+  const args: string[] = [
+    "xcresulttool",
+     "get",
+     "--path",
+     file,
+     "--format",
+     "json"
+  ];
+
+  await exec.exec('xcrun', args, options);
+  console.log(`Output: ${output}`);
+}
