76c96c4034b08279ea39061686ec5268daef11ad
Thomas Bartelmess <tbartelmess@marketcircle.com>
2021-03-24 20:00:23 -0400

    Run tool

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import * as core from '@actions/core'
-
+import * as xcresulttool from './xcresulttool'
 async function run(): Promise<void> {
   try {
     const inputFile: string = core.getInput('results')
     core.info(`Analyzing ${inputFile} ...`)
-
+    await xcresulttool.transformXCodeResults(inputFile)
   } catch (error) {
     core.setFailed(error.message)
   }
